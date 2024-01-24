3cacfc555360d0e7850d566952bbde0ffd5d7d5c
francieric <43497719+francieric@users.noreply.github.com>
2024-01-24 18:37:12 -0300

    retirado

--- ingestion_chroma.py
+++ ingestion_chroma.py
@@ -8,9 +8,7 @@ import os
 
 load_dotenv()
 
-openai_api_key=os.getenv('OPENAI_API_KEY', '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')
 
-os.environ["OPENAI_API_KEY"] = "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"
 
 loader = DirectoryLoader('./docs', glob="**/*.pdf", show_progress=True)
 
